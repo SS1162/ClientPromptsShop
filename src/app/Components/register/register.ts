3d3a70a2d54f224b41151cafddecc9e59fd07eef
SS1162 <s058329162@gmail.com>
2025-12-21 13:52:14 +0200

    upload the project

---
+++ src/app/Components/register/register.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-register',
+  imports: [ButtonModule],
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
+})
+export class Register {
+
+}
+gi
